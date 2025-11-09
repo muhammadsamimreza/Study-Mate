cefbadab11625164c7ab9d982ced7770d4069550
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-11-09 19:15:57 +0600

    Footer created

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
+import logo from "/logo.jpg"
+import { Link } from "react-router";
+import Container from "../Container/Container";
 
 const Footer = () => {
-    return (
-        <div>
-            Tis is Footer
+  return (
+    <footer className="bg-yellow-50 text-gray-800 py-10 px-6 mt-10  shadow-inner">
+      <Container>
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center text-center md:text-left">
+          <div>
+            <Link to="/" className="flex gap-5 justify-center md:justify-start items-center text-3xl font-bold text-amber-500 mb-3">
+             <img className="w-8" src={logo} alt="" /> STUDY MATE
+            </Link>
+            <p className="text-sm text-gray-600 leading-relaxed">
+              Your smart study companion — helping you stay organized,
+              motivated, and connected while learning.
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-xl font-semibold mb-3 text-gray-700">
+              Quick Links
+            </h3>
+            <ul className="space-y-2">
+              <li>
+                <a href="#" className="hover:text-amber-500">
+                  Terms & Conditions
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-amber-500">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-amber-500">
+                  Help & Support
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h3 className="text-xl font-semibold mb-3 text-gray-700">
+              Follow Us
+            </h3>
+            <div className="flex justify-center md:justify-start gap-5 text-amber-500 text-2xl">
+              <a href="#">
+                <FaFacebookF className="hover:scale-110 transition-transform" />
+              </a>
+              <a href="#">
+                <FaInstagram className="hover:scale-110 transition-transform" />
+              </a>
+              <a href="#">
+                <FaYoutube className="hover:scale-110 transition-transform" />
+              </a>
+            </div>
+          </div>
         </div>
-    );
+
+        <div className="border-t border-gray-300 mt-10 pt-4 text-center text-sm text-gray-500">
+          © {new Date().getFullYear()} STUDY MATE. All Rights Reserved.
+        </div>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
